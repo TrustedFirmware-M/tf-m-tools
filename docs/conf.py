7e3772b096e00986524101b621d6cb945c95309e
Anton Komlev <anton.komlev@arm.com>
2026-03-31 18:01:46 +0100

    Build: Update Sphinx and python configuration
    - Adjust documentation configuration with the main repository as a
      followup of this change:
      https://review.trustedfirmware.org/c/TF-M/trusted-firmware-m/+/49187
    
    Change-Id: Ia48c40de1152e5705e8ef97b6ea64c79ef21d751
    Signed-off-by: Anton Komlev <anton.komlev@arm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ except:
 
 extensions = [
     'sphinx.ext.imgmath',
-    'm2r2', #Support markdown files. Needed for external code.
+    'myst_parser', #Support markdown files. Needed for external code.
     'sphinx.ext.autosectionlabel', #Make sphinx generate a label for each section
     'sphinxcontrib.plantuml', #Add support for PlantUML drawings
     'sphinxcontrib.rsvgconverter', #Add support for SVG to PDF
@@ -105,11 +105,10 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'collapse_navigation' : False,
     'prev_next_buttons_location' : None,   # Hide Prev and Next buttons
-    'display_version': True,    # Show version under logo
+    "version_selector": True,
     'sticky_navigation': True,
     'navigation_depth': 3,
 }
-
 # Remove the "View page source" link from the top of docs pages
 html_show_sourcelink = False
 
